ba69bc2a2764ef9da495c34145b0d7d4f847cea6
whflove1004 <whflove1004@126.com>
2017-12-06 09:51:39 +0800

    增加md5加密方法

--- jm/pjm.py
+++ jm/pjm.py
@@ -4,11 +4,11 @@ import xlrd
 import xlwt
 import os
 import hashlib
+import md5
 import pandas as pd
-import openpyxl
 dir_path = os.path.dirname(__file__)
 excle_path = os.path.join(dir_path, '360z.xlsx')
-res_path= os.path.join(dir_path, '360z_res1.xlsx')
+res_path= os.path.join(dir_path, '360z_res.xlsx')
 wb = xlwt.Workbook()
 ws = wb.add_sheet('res')
 def to_hash(st):
@@ -16,15 +16,22 @@ def to_hash(st):
     m.update(st)
     sha = m.hexdigest()
     return sha
+
+def to_md5(st):
+    import hashlib
+    m = hashlib.md5()   
+    m.update(st)
+    return m.hexdigest()
+
 def read_to_excle():
     phone=[]
     id_card = []
     df = pd.read_excel(excle_path)
     for i in df['mobile']:
-        a = to_hash(str(int(i)))
+        a = to_md5(str(int(i)))
         phone.append(a)
     for j in df['id_card']:
-        b = to_hash(str(j))
+        b = to_md5(str(j))
         id_card.append(b)
     df['mobile'] = phone
     df['id_card'] = id_card
